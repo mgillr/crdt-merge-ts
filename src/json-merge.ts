863965166e217d09e1681feeb1e9846ee6ebc7ac
Ryan Gillespie <36207807+mgillr@users.noreply.github.com>
2026-03-27 02:35:09 +0100

    Add copyright header

--- src/json-merge.ts
+++ src/json-merge.ts
@@ -1,3 +1,8 @@
+// Copyright 2026 Ryan Gillespie
+// SPDX-License-Identifier: Apache-2.0
+//
+// Commercial licensing: data@optitransfer.ch, rgillespie83@icloud.com
+
 /**
  * Deep conflict-free JSON/object merge using CRDT semantics.
  *
